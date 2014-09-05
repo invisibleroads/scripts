9c03e3f9a1d51e1a5424af3aabd1530f1f9b47ab
Roy Hyunjin Han <rhh@crosscompute.com>
2014-09-05 14:48:07 -0400

    Upgrade make-password

--- make-password.py
+++ make-password.py
@@ -1,6 +1,30 @@
 #!/usr/bin/env python
+from argparse import ArgumentParser
 from random import choice
-from string import digits, letters
+from string import digits, letters, punctuation
 
 
-print(''.join(choice(digits + letters) for x in xrange(32)))
+def make_random_string(alphabet, length):
+    return ''.join(choice(alphabet) for x in xrange(length))
+
+
+def get_alphabet(code):
+    alphabet = ''
+    if 'd' in code:
+        alphabet += digits
+    if 'l' in code:
+        alphabet += letters
+    if 's' in code:
+        alphabet += ' '
+    if 'p' in code:
+        alphabet += punctuation
+    return alphabet
+
+
+if __name__ == '__main__':
+    argument_parser = ArgumentParser()
+    argument_parser.add_argument('--length', '-l', metavar='32', type=int, default=32)
+    argument_parser.add_argument('--code', '-c', metavar='dlsp', default='dl')
+    args = argument_parser.parse_args()
+    alphabet = get_alphabet(args.code)
+    print make_random_string(alphabet, args.length)
