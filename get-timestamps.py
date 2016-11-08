7a7d230d8b6daa5d32e3b2fd07b64d52cf455b7d
Roy Hyunjin Han <rhh@crosscompute.com>
2016-11-08 11:43:58 -0500

    Use 30 minutes

--- get-timestamps.py
+++ get-timestamps.py
@@ -5,7 +5,7 @@ import sys
 try:
     interval_in_minutes = int(sys.argv[1])
 except (IndexError, ValueError):
-    interval_in_minutes = 60
+    interval_in_minutes = 30
 timestamp1 = datetime.datetime.now()
 timestamp2 = timestamp1 + datetime.timedelta(minutes=interval_in_minutes)
 timestamp_format = '%Y%m%d-%H%M'
