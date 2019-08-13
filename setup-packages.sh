2ff44922588edf08fffba216bcf22b27d1ceae10
Roy Hyunjin Han <rhh@crosscompute.com>
2019-08-13 11:07:45 -0400

    Let user specify ENV_NAME

--- setup-packages.sh
+++ setup-packages.sh
@@ -1,4 +1,10 @@
-VIRTUALENV_FOLDER=~/.virtualenvs/crosscompute
+if [ -n "$1" ]; then
+    ENV_NAME=`basename $1`
+else
+    ENV_NAME=crosscompute
+fi
+echo "Using ENV_NAME=$ENV_NAME"
+VIRTUALENV_FOLDER=~/.virtualenvs/$ENV_NAME
 
 if [ ! -d $VIRTUALENV_FOLDER ]; then
     virtualenv $VIRTUALENV_FOLDER -p $(which python3) --system-site-packages
