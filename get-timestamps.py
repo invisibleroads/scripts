bdcd2a54e7c857ca152cdabf5cfa65d17faf7683
Roy Hyunjin Han <rhh@crosscompute.com>
2020-04-07 15:58:40 -0400

    Add day of week

--- get-timestamps.py
+++ get-timestamps.py
@@ -21,7 +21,8 @@ if __name__ == '__main__':
 
     timestamp1, timestamp2 = get_timestamps(args.interval_in_minutes)
     timestamp_format = '%Y%m%d-%H%M'
-    print('%s - %s: %s minutes' % (
+    print('%s %s - %s: %s minutes' % (
+        timestamp1.strftime('%A'),
         timestamp1.strftime(timestamp_format),
         timestamp2.strftime(timestamp_format),
         args.interval_in_minutes))
