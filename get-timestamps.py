b242dd687310174dbfa5632a38a155442ca64122
Roy Hyunjin Han <rhh@crosscompute.com>
2020-09-22 19:44:38 -0400

    Save timestamps to clipboard

--- get-timestamps.py
+++ get-timestamps.py
@@ -21,8 +21,14 @@ if __name__ == '__main__':
 
     timestamp1, timestamp2 = get_timestamps(args.interval_in_minutes)
     timestamp_format = '%Y%m%d-%H%M'
-    print('%s %s - %s: %s minutes' % (
+    timestamp_text = '%s %s - %s: %s minutes' % (
         timestamp1.strftime('%A'),
         timestamp1.strftime(timestamp_format),
         timestamp2.strftime(timestamp_format),
-        args.interval_in_minutes))
+        args.interval_in_minutes)
+    print(timestamp_text)
+    try:
+        import pyperclip
+        pyperclip.copy(timestamp_text)
+    except Exception:
+        pass
