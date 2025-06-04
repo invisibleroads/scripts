57f0a9151fe9bcebf678d766bc4682f47ae3724e
Roy Hyunjin Han <rhh@crosscompute.com>
2025-06-04 16:24:49 -0400

    Use ruff

--- make-password.py
+++ make-password.py
@@ -25,16 +25,15 @@ def get_alphabet(code):
 
 if __name__ == '__main__':
     argument_parser = ArgumentParser()
-    argument_parser.add_argument(
-        '--length', '-l', metavar='32', type=int, default=32)
-    argument_parser.add_argument(
-        '--code', '-c', metavar='dlusp', default='dlu')
+    argument_parser.add_argument('--length', '-l', metavar='32', type=int, default=32)
+    argument_parser.add_argument('--code', '-c', metavar='dlusp', default='dlu')
     args = argument_parser.parse_args()
     alphabet = get_alphabet(args.code)
     random_string = make_random_string(alphabet, args.length)
     print(random_string)
     try:
         import pyperclip
+
         pyperclip.copy(random_string)
     except Exception:
         print('sudo dnf -y install xsel')
